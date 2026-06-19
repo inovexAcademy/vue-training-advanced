e64836ba6f110d67a6aa70b5b42e7baafe211bc9
Daniel Eckelt <crymis8@gmail.com>
2026-06-19 10:43:02 +0200

    fix: eslint parsing error

--- eslint.config.js
+++ eslint.config.js
@@ -3,23 +3,55 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginVue from 'eslint-plugin-vue';
 import globals from 'globals';
 import typescriptEslint from 'typescript-eslint';
+import vueParser from 'vue-eslint-parser';
 
-export default typescriptEslint.config(
-  { ignores: ['*.d.ts', '**/coverage', '**/dist', '**/api'] },
+const typescriptConfig = typescriptEslint.config(
   {
-    extends: [
-      eslint.configs.recommended,
-      ...typescriptEslint.configs.recommended,
-      ...eslintPluginVue.configs['flat/recommended'],
-    ],
-    files: ['**/*.{ts,vue}'],
+    extends: [eslint.configs.recommended],
+    files: ['**/*.{js,mjs}'],
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
       globals: globals.browser,
+    },
+  },
+  ...typescriptEslint.configs.recommended
+);
+
+export default [
+  ...typescriptConfig,
+  {
+    files: ['**/*.{ts,mts}'],
+    languageOptions: {
+      parser: typescriptEslint.parser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  {
+    ignores: ['*.d.ts', '**/coverage', '**/dist', '**/api'],
+  },
+  ...eslintPluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
       parserOptions: {
         parser: typescriptEslint.parser,
+        sourceType: 'module',
+        ecmaVersion: 'latest',
       },
+      globals: globals.browser,
     },
     rules: {
       'vue/no-undef-components': [
@@ -37,5 +69,5 @@ export default typescriptEslint.config(
       ],
     },
   },
-  eslintConfigPrettier
-);
+  eslintConfigPrettier,
+];
