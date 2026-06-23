662bace6bc8527b2b755b347be68e1be49c58f8c
Daniel Eckelt <crymis8@gmail.com>
2026-06-23 11:49:39 +0200

    docs: add hint for ex2 for store usage in router

--- _exercises/02-Advanced-Routing/src/router/router.ts
+++ _exercises/02-Advanced-Routing/src/router/router.ts
@@ -5,6 +5,9 @@ import HomeView from '@/views/HomeView.vue';
 import NotFoundView from '@/views/NotFoundView.vue';
 import ProductView from '@/views/ProductView.vue';
 
+// ⚠️ Beware of using the authStore directly in the router file as pinia must be initialized first.
+// You can access a store within a guard function.
+
 export const routes = [
   { path: '/', component: HomeView, name: 'Home' },
   { path: '/checkout', component: CheckoutView, name: 'Checkout' },
